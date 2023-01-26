d0751905203f4e63bbdc23554282641ee281cf21
DmitryPinegin <snobmsi@gmail.com>
2023-01-26 11:47:51 +0300

    update

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -10,13 +10,13 @@ public class App {
         for (int i = 0; i < symbols.length(); i++) {
             symbol.add(String.valueOf(symbols.charAt(i)));
         }
-        List<String> arrword = new ArrayList<>();
+        List<String> lowerCaseChars = new ArrayList<>();
         for (int y = 0; y < word.length(); y++) {
             var cha = String.valueOf(word.charAt(y));
-            arrword.add(cha.toLowerCase());
+            lowerCaseChars.add(cha.toLowerCase());
         }
 
-        for (String d : arrword) {
+        for (String d : lowerCaseChars) {
             if (!symbol.contains(d)) {
                 return false;
             } else if (symbol.contains(d)) {

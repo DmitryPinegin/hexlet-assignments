47b57b1c47c7a85dc8651ac66af4227b4422daf9
DmitryPinegin <snobmsi@gmail.com>
2023-01-27 21:07:16 +0300

    update

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -7,30 +7,31 @@ import java.util.Map;
 class App {
     public static Map getWordCount(String sentence) {
         Map<String, Integer> wordCount = new HashMap<>();
-        List<String> list = new ArrayList<>();
         String[] separatedSentence = sentence.split(" ");
-        var i = 1;
         for (String l : separatedSentence) {
-            list.add(l);
-            for (String s : list) {
-                if(list.contains(s)) {
-                    i++;
-                    wordCount.put(s,i);
-
-                } else {
-                    wordCount.put(s,i);
-                }
+            if(sentence.equals("")) {
+                return wordCount;
+            } else if (wordCount.containsKey(l)) {
+                wordCount.put(l, wordCount.get(l) + 1);
+            } else {
+                wordCount.put(l, 1);
             }
         }
         return wordCount;
     }
-    public static String toString(Map<String, Integer> words) {
-        String s = null;
-        for(Map.Entry<String, Integer> word : words.entrySet()) {
-            s = "{\n" + "  " + word.getKey() + ":  " + word.getValue() + "\n";
-
-        } System.out.println(s);
-        return s;
+    public static String toString(Map wordCount) {
+        String[] res = new String[wordCount.size()];
+        int i = 0;
+        String result = "";
+        for (Object s : wordCount.keySet()) {
+            res[i] = "\n" + "  " + s + ": " + wordCount.get(s) + "\n";
+            i++;
+        }
+        for (String r : res) {
+            result += r;
+        }
+        return  "\n  " + result + "  \n";
     }
 }
 //END
+
